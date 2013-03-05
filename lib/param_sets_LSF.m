051eeeda056088d434ae71fc78643536a99bf221
Stefano Balietti <futur.dorko@gmail.com>
2013-03-05 23:28:13 +0100

    removed email sending from each job

--- lib/param_sets_LSF.m
+++ lib/param_sets_LSF.m
@@ -13,7 +13,8 @@ dumpFolder = [ dumpDir simName];
 sched = findResource('scheduler','type','lsf');
 emails = 'sbalietti@ethz.ch';
 %submitArgs = ['-o ' logFolder '/' simName '.log -u ' emails];
-submitArgs = ['-o ' logFolder '/' simName '.log -B'];
+%submitArgs = ['-o ' logFolder '/' simName '.log -B']; -B / -N sends email
+submitArgs = ['-o ' logFolder '/' simName '.log'];
 set(sched, 'SubmitArguments',submitArgs);
 set(sched, 'DataLocation', [logFolder '/']);
 
