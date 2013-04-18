83ac3708e595c556ca2a07b5e427538b94417039
Stefano Balietti <futur.dorko@gmail.com>
2013-04-18 14:48:12 +0200

    exactcorner noisev

--- conf/base.m
+++ conf/base.m
@@ -4,7 +4,7 @@
 
 % GLOBAL Conf
 
-simName = 'truth_middle_alpha_Rleft_sigma_av1_noise_v';
+simName = 'truth_exactcorner_alpha_Rleft_sigma_av1_noise_v';
 dumpDir = '/cluster/work/scr4/balistef/'; % dump
 
 VIDEO = 0;
@@ -77,7 +77,7 @@ for i=2:numel(hGrid)
 end
 truths = [repmat(hGrid,1,nPointsGrid); vGrid];
 
-truths = [0.5; 0.5];
+truths = [0; 0];
 
 truths
 
