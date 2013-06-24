118bdf79304a8e04770afda1e40b5dc86895855c
Stefano Balietti <futur.dorko@gmail.com>
2013-06-25 00:12:01 +0200

    bugfix

--- aggregate_fun.m
+++ aggregate_fun.m
@@ -122,42 +122,42 @@ function [] = temporalysis_fun(dumpDir, subDir)
     df = N - 1;
 
     % Computing g_global stats
-    t_count_avg = global_count_sum / N; 
-    t_count_sd = sqrt(((global_count_sumsquared - ((global_count_sum).^2 / N))) / df);
+    t_count_avg = g_global_count_sum / N; 
+    t_count_sd = sqrt(((g_global_count_sumsquared - ((g_global_count_sum).^2 / N))) / df);
     t_count_se = t_count_sd / sqrt(N);  
     t_count_ci = t_count_se * tquant(CI_INT, df);
     
-    t_cover_avg = global_coverage_sum / N; 
-    t_cover_sd = sqrt(((global_coverage_sumsquared - ((global_coverage_sum).^2 / N))) / df);
+    t_cover_avg = g_global_coverage_sum / N; 
+    t_cover_sd = sqrt(((g_global_coverage_sumsquared - ((g_global_coverage_sum).^2 / N))) / df);
     t_cover_se = t_cover_sd / sqrt(N);  
     t_cover_ci = t_cover_se * tquant(CI_INT, df);
     
-    t_cover_cum_avg = global_coverage_cum_sum / N; 
-    t_cover_cum_sd = sqrt(((global_coverage_cum_sumsquared - ((global_coverage_cum_sum).^2 / N))) / df);
+    t_cover_cum_avg = g_global_coverage_cum_sum / N; 
+    t_cover_cum_sd = sqrt(((g_global_coverage_cum_sumsquared - ((g_global_coverage_cum_sum).^2 / N))) / df);
     t_cover_cum_se = t_cover_cum_sd / sqrt(N);  
     t_cover_cum_ci = t_cover_cum_se * tquant(CI_INT, df);
     
-    t_speed_avg = global_speed_sum / N; 
-    t_speed_sd = sqrt(((global_speed_sumsquared - ((global_speed_sum).^2 / N))) / df);
+    t_speed_avg = g_global_speed_sum / N; 
+    t_speed_sd = sqrt(((g_global_speed_sumsquared - ((g_global_speed_sum).^2 / N))) / df);
     t_speed_se = t_speed_sd / sqrt(N);  
     t_speed_ci = t_speed_se * tquant(CI_INT, df);
     
-    t_move_avg = global_move_sum / N; 
-    t_move_sd = sqrt(((global_move_sumsquared - ((global_move_sum).^2 / N))) / df);
+    t_move_avg = g_global_move_sum / N; 
+    t_move_sd = sqrt(((g_global_move_sumsquared - ((g_global_move_sum).^2 / N))) / df);
     t_move_se = t_move_sd / sqrt(N);  
     t_move_ci = t_move_se * tquant(CI_INT, df);
     
-    t_size_avg = global_size_sum / N; 
-    t_size_sd = sqrt(((global_size_sumsquared - ((global_size_sum).^2 / N))) / df);
+    t_size_avg = g_global_size_sum / N; 
+    t_size_sd = sqrt(((g_global_size_sumsquared - ((g_global_size_sum).^2 / N))) / df);
     t_size_se = t_size_sd / sqrt(N);  
     t_size_ci = t_size_se * tquant(CI_INT, df);
 
-    t_fromtruth_avg = global_fromtruth_sum / N; 
-    t_fromtruth_sd = sqrt(((global_fromtruth_sumsquared - ((global_fromtruth_sum).^2 / N))) / df);
+    t_fromtruth_avg = g_global_fromtruth_sum / N; 
+    t_fromtruth_sd = sqrt(((g_global_fromtruth_sumsquared - ((g_global_fromtruth_sum).^2 / N))) / df);
     t_fromtruth_se = t_fromtruth_sd / sqrt(N);  
     t_fromtruth_ci = t_fromtruth_se * tquant(CI_INT, df);
     
-    dataFileName = [dumpDir 'clusters_macro_avg.csv'];
+    dataFileName = [DUMPDIR 'clusters_macro_avg.csv'];
     write_csv_headers(dataFileName, headers_clusters_macro_avg);
     fidClustersMacroAvg = fopen(dataFileName,'a');
     
