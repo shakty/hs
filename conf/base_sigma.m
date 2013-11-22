59f212534ec9c100c21619b02e27cd96995dc2ad
Stefano Balietti <futur.dorko@gmail.com>
2013-11-22 15:09:35 +0100

    attrZero R0 S10

--- conf/base_sigma.m
+++ conf/base_sigma.m
@@ -8,8 +8,8 @@ clc;
 
 % always av1
 % attr  _ noise _ seedType _ update _  truth _ parameter sweep _ nAgents _ forceOnV _ size 
-simName = 'attrLinear_nav_rndseeds_rndseq_tm_R0_n100_fv0_s10';
-dumpDir = '/cluster/work/scr1/balistef/';
+simName = 'attrZero_nav_rndseeds_rndseq_tm_R0_n100_fv0_s10';
+dumpDir = '/cluster/work/scr5/balistef/';
 %dumpDir = 'dump/';
 bsubWD = '/cluster/home/gess/balistef/matlab/hsnew/';
 
@@ -103,7 +103,7 @@ attr_hard_to_find = 5;
 attr_wide_funnel = 6;
 attr_gentle_landing = 7;
 
-attrtype = 2;
+attrtype = 0;
 
 % PLOT TYPE
 plot_cross = 0;
