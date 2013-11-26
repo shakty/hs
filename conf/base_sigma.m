f113e8f795ce7e55f3b21d5e1059547d6fa00ee2
Stefano Balietti <futur.dorko@gmail.com>
2013-11-26 14:05:23 +0100

    Attr Linear nav t(.5 .5)

--- conf/base_sigma.m
+++ conf/base_sigma.m
@@ -8,8 +8,8 @@ clc;
 
 % always av1
 % attr  _ noise _ seedType _ update _  truth _ parameter sweep _ nAgents _ forceOnV _ size 
-simName = 'attrMillean_nav_rndseeds_rndseq_tm_RClean_n100_fv0_s1';
-dumpDir = '/cluster/work/scr4/balistef/';
+simName = 'attrLinear_nav_rndseeds_rndseq_tm_RClean_n100_fv0_s1';
+dumpDir = '/cluster/work/scr5/balistef/';
 %dumpDir = 'dump/';
 bsubWD = '/cluster/home/gess/balistef/matlab/hsnew/';
 
@@ -103,7 +103,7 @@ attr_hard_to_find = 5;
 attr_wide_funnel = 6;
 attr_gentle_landing = 7;
 
-attrtype = 4;
+attrtype = 2;
 
 % PLOT TYPE
 plot_cross = 0;
