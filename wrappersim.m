42c121f0f96e621a5cc904e8cda82548b2524a73
Stefano Balietti <futur.dorko@gmail.com>
2013-11-29 17:31:25 +0100

    added path in wrappersim as well

--- wrappersim.m
+++ wrappersim.m
@@ -1,4 +1,8 @@
 function wrappersim( paramArgs )
+    
+    %% Add other directories to path
+    path(path,'util/'); % Help functions
+    path(path,'lib/'); % Help functions
 
     for i=1:length(paramArgs)
         simulation(paramArgs{i})
