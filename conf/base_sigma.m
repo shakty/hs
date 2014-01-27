60284b856ca23b299bd340d405d97d4f1ceef708
Stefano Balietti <futur.dorko@gmail.com>
2014-01-28 00:01:24 +0100

    epsilon_sigma_vscaling

--- conf/base_sigma.m
+++ conf/base_sigma.m
@@ -8,8 +8,8 @@ clc;
 
 % always av1
 % attr  _ noise _ seedType _ update _  truth _ parameter sweep _ nAgents _ forceOnV _ size 
-simName = 'final_alpha';
-dumpDir = '/cluster/work/scr4/balistef/';
+simName = 'final_epsilon_sigma_vscaling';
+dumpDir = '/cluster/work/scr3/balistef/';
 
 % we have two because we can save the new configuration in a separate
 % folder analyze an old one without deleting its conf files.
@@ -51,7 +51,7 @@ ideas_space_dims = [2]; % dimension of ideas space
 % ks the bigger the less groups
 
 % VELOCITY 
-alphas = [0.01:0.01:0.99];  % weighting of velocity terms
+alphas = [0.5];  % weighting of velocity terms
 Rs     = [0.03, 0.3]; % cut-off radius
          
 % ATTRACTIVE AND REPULSIVE FORCES
@@ -69,13 +69,13 @@ d1s    = [1];       	% Express the range of the interaction force (exponent divi
 taus   = [1];     		% coupling coefficient (divisor)
 
 % MEASURAMENT NOISE (position)
-epsilons = 0.1; % [0:0.1:0.5]; % 0.1; % Std. deviation of white noise term
+epsilons = [0:0.1:0.5]; % 0.1; % Std. deviation of white noise term
 
 % NOISE on APPROACH (INDIVIDUALIZATION) (direction)
-sigmas = 0.01; % [0:0.01:0.05];   % Std. deviation of white noise term
+sigmas = [0:0.01:0.05]; % 0.01; % Std. deviation of white noise term
 
 % INITIIAL VELOCITIES OF SCIENTISTS
-vScalings = [1]; % [0.2, 0.5, 1, 2, 10, 100]; % Scaling factor for initial (random) velocities
+vScalings = [0.2, 0.5, 1, 2, 10, 100]; % Scaling factor for initial (random) velocities
 
 % INITIAL POSITIONS OF SCIENTISTS
 nClusters = [0];    	% number of clusters of the initial positions
