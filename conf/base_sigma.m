f7b9a13246f40c3381344cc5ac2acf69685ae713
Stefano Balietti <futur.dorko@gmail.com>
2014-02-10 21:40:46 +0100

    real cl vs cls

--- conf/base_sigma.m
+++ conf/base_sigma.m
@@ -8,8 +8,8 @@ clc;
 
 % always av1
 % attr  _ noise _ seedType _ update _  truth _ parameter sweep _ nAgents _ forceOnV _ size 
-simName = 'final_tau_20000';
-dumpDir = '/cluster/work/scr1/balistef/';
+simName = 'clusterVsClusters';
+dumpDir = '/cluster/work/scr6/balistef/';
 
 % we have two because we can save the new configuration in a separate
 % folder analyze an old one without deleting its conf files.
@@ -33,7 +33,7 @@ SHOW_POTENTIAL = 0;
 nRuns = 10;              % Number of simulation-runs with same param set
 
 dts = [0.01];           % time_step
-t_ends = [200];          % running time
+t_ends = [20];          % running time
 
 n_agents = [100];       % number of agents
 
@@ -66,7 +66,7 @@ d1s    = [1];       	% Express the range of the interaction force (exponent divi
 
 
 % HOW EASY IS TO FIND THE TRUTH (
-taus   = [1:100];     		% coupling coefficient (divisor)
+taus   = [1];     		% coupling coefficient (divisor)
 
 % MEASURAMENT NOISE (position)
 epsilons = [0.1]; % 0.1; % Std. deviation of white noise term
@@ -78,9 +78,9 @@ sigmas = [0.01]; % 0.01; % Std. deviation of white noise term
 vScalings = [1]; % [0.2, 1, 2, 5, 10]; %[0.2:0.2:10]; % Scaling factor for initial (random) velocities
 
 % INITIAL POSITIONS OF SCIENTISTS
-nClusters = [0];    	% number of clusters of the initial positions
+nClusters = [1:30];    	% number of clusters of the initial positions
 clusterTightness = [0.05]; % Tightness of clusters
-clustersInCircleOfRadius = [-1];
+clustersInCircleOfRadius = [-1 0.4];
 
 % TRUTH POSITION
 % Generate Truth Vector for 2D Truth
